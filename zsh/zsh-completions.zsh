c94b49c576e69d1e7163c20b78b4483260193f67
Marcus LaFerrera <marcus@randomhack.org>
2014-12-06 16:38:25 -0500

    Added gitconfig.user

--- zsh/zsh-completions.zsh
+++ zsh/zsh-completions.zsh
@@ -1,3 +1,6 @@
 # Fix for zsh-completions on OS X
 # https://unix.stackexchange.com/questions/126785/zsh-completion-of-brew-formulas
+# You may also need to force rebuild `zcompdump`:
+#
+#   rm -f ~/.zcompdump; compinit
 fpath=(/usr/local/share/zsh-completions $fpath)
