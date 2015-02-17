1ae407639e3a6b3224b888fdc1784371c9cd22ab
Marcus LaFerrera <marcus@randomhack.org>
2015-02-16 21:54:51 -0500

    Added go installation to install process. Needs to be moved into Rakefile.

--- install.sh
+++ install.sh
@@ -6,6 +6,32 @@ if [ ! -d "$HOME/.yadr" ]; then
     cd "$HOME/.yadr"
     [ "$1" = "ask" ] && export ASK="true"
     rake install
+
+    # gocode is used by many editors to provide intellisense
+    go get github.com/nsf/gocode
+
+    # godoc is used by many editors to provide documentation support
+    go get golang.org/x/tools/cmd/godoc
+
+    #vet should be run after every build to check your code
+    go get golang.org/x/tools/cmd/vet
+
+    # goimports is something you should run when saving code to fill in import paths
+    go get golang.org/x/tools/cmd/goimports
+
+    # gorename is used by many editors to provide identifier rename support
+    go get golang.org/x/tools/cmd/gorename
+
+    # oracle is a tool that help with code navigation and search
+    go get golang.org/x/tools/cmd/oracle
+
+    # gotype is used by many editors to provide Go support
+    go get golang.org/x/tools/cmd/gotype
+
+    # golint should be run after every build to check your code
+    go get github.com/golang/lint/golint
+
 else
     echo "YADR is already installed"
 fi
+
