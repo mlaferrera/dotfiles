8151951cf75cc4a39913ad35eae6148ed84fd072
Marcus LaFerrera <marcus@randomhack.org>
2016-04-27 15:34:21 -0400

    Fixed insecure directories issues with zsh completions.

--- zsh/zsh-completions.zsh
+++ zsh/zsh-completions.zsh
@@ -2,6 +2,6 @@
 # https://unix.stackexchange.com/questions/126785/zsh-completion-of-brew-formulas
 # You may also need to force rebuild `zcompdump`:
 #
-#   rm -f ~/.zcompdump; compinit
+rm -f ~/.zcompdump #; compinit
 fpath=(/usr/local/share/zsh-completions $fpath)
-compinit -i
+compinit -u
